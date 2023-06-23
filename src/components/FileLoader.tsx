d82fcdfce852009dad39d6d118bfab375f19dca3
a.rashkov <a.rashkov@astoundcommerce.com>
2023-06-23 11:37:16 +0300

    Implements delete objects

--- src/components/FileLoader.tsx
+++ src/components/FileLoader.tsx
@@ -6,6 +6,7 @@ import {
   ListObjectsV2Command,
   ListObjectsV2CommandOutput,
   PutObjectCommand,
+  DeleteObjectsCommand,
 } from '@aws-sdk/client-s3';
 
 const ACCESS_KEY_ID: string = process.env.REACT_APP_ACCESS_KEY_ID || '';
@@ -14,9 +15,13 @@ const ACCESS_REGION: string = process.env.REACT_APP_REGION || '';
 const BUCKET: string = process.env.REACT_APP_BUCKET || '';
 
 // Custom interfaces
-interface bucketObject {
-  key: string | undefined;
-  lastModified: string | undefined;
+interface IBucketObject {
+  Key: string;
+  lastModified: string;
+}
+
+class BucketObject implements IBucketObject {
+  public constructor(public Key: string, public lastModified: string) {}
 }
 
 type FormValues = {
@@ -36,9 +41,13 @@ const client = new S3Client(config);
 const FileLoader: React.FC = () => {
   // List all objects in a bucket
   // TODO List objects in a bucket starting with some prefix.
-  const [bucketObjects, setBucketObjects] = useState<bucketObject[]>([]);
+  const [bucketObjects, setBucketObjects] = useState<IBucketObject[]>([]);
 
   const handleObjectList = async () => {
+    // Note it will list the first 1000 objects
+    // You should check for .NextContinuationToken
+    // https://www.codemzy.com/blog/delete-s3-folder-nodejs
+
     const command = new ListObjectsV2Command({
       Bucket: BUCKET,
     });
@@ -47,14 +56,19 @@ const FileLoader: React.FC = () => {
     const response: ListObjectsV2CommandOutput = await client.send(command);
 
     if (response.Contents) {
-      const awsBucketObjects: bucketObject[] = response.Contents.map((item) => {
-        return {
-          key: item.Key,
-          lastModified: item.LastModified?.toISOString(),
-        };
-      });
+      const awsBucketObjects: IBucketObject[] = response.Contents.map(
+        (item) => {
+          return new BucketObject(item.Key!, item.LastModified?.toISOString()!);
+        }
+      );
 
       setBucketObjects(awsBucketObjects);
+      return;
+    }
+
+    // No objects in the bucket so reset the state if it was not empty
+    if (bucketObjects.length) {
+      setBucketObjects([]);
     }
   };
 
@@ -91,8 +105,39 @@ const FileLoader: React.FC = () => {
       console.error(err);
     }
   };
-
   // EO: Creating an object with a given name
+
+  // Deleting objects
+  const handleObjectDelete = async (deletedObject: IBucketObject) => {
+    // recursive delete
+    const isFolder = deletedObject?.Key?.endsWith('/');
+
+    let deletedObjects: IBucketObject[] | [] = [];
+
+    if (isFolder) {
+      deletedObjects = bucketObjects.filter((bucketObject) =>
+        bucketObject?.Key?.startsWith(deletedObject?.Key!)
+      );
+    } else {
+      deletedObjects = [deletedObject];
+    }
+
+    const command = new DeleteObjectsCommand({
+      Bucket: BUCKET,
+      Delete: {
+        Objects: deletedObjects,
+      },
+    });
+
+    try {
+      const response = await client.send(command);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  // EO: Deleting objects
+
   return (
     <div>
       <div>
@@ -107,7 +152,10 @@ const FileLoader: React.FC = () => {
         <button onClick={handleObjectList}>List Objects</button>
         <div>
           {bucketObjects.map((item) => (
-            <li key={item.key}>{item.key}</li>
+            <li key={item.Key}>
+              <span>{item.Key}</span>
+              <button onClick={() => handleObjectDelete(item)}>Delete</button>
+            </li>
           ))}
         </div>
       </div>
