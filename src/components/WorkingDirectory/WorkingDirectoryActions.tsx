1f63461b4b6c474eb9faf562e80743b32f1e9334
a.rashkov <a.rashkov@astoundcommerce.com>
2023-06-28 16:50:58 +0300

    Styles working dir actions

--- src/components/WorkingDirectory/WorkingDirectoryActions.tsx
+++ src/components/WorkingDirectory/WorkingDirectoryActions.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Button from '../UI/Button/Button';
 
+import { ReactComponent as TrashSVG } from '../../assets/trash.svg';
+import { ReactComponent as PlusSVG } from '../../assets/plus.svg';
 import classes from './WorkingDirectoryActions.module.css';
 
 type WorkingDirActionsProps = {
@@ -9,18 +11,23 @@ type WorkingDirActionsProps = {
 };
 
 const WorkingDirActions: React.FC<WorkingDirActionsProps> = ({ className }) => {
-  const handleCreateFolder = () => {};
-
   return (
     <div
       className={`${classes['working-directory-actions']} ${
         className ? className : ''
       }`}
     >
-      <Button type='button'>+ Folder</Button>
-      <Button type='button'>+ File</Button>
-      <Button type='button' disabled>
-        Delete Selected
+      <Button type='button'>
+        <PlusSVG className={classes['working-directory-actions--icon']} />
+        Folder
+      </Button>
+      <Button type='button'>
+        <PlusSVG className={classes['working-directory-actions--icon']} />
+        File
+      </Button>
+      <Button type='button'>
+        <TrashSVG className={classes['working-directory-actions--icon']} />
+        Delete
       </Button>
     </div>
   );
