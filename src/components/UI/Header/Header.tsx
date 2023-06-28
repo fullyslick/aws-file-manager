8a509de32f979d643542b1c519cb33cfa15341a7
a.rashkov <a.rashkov@astoundcommerce.com>
2023-06-28 13:42:09 +0300

    Styles the Header

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import classes from './Header.module.css';
+
+import { ReactComponent as LogoSVG } from '../../../assets/logo.svg';
+import { ReactComponent as ServerCloseSVG } from '../../../assets/server-close-icon.svg';
+
 const Header: React.FC = () => {
-  return <>Header</>;
+  return (
+    <header className={classes.header}>
+      <div className={classes['header__logo-wrapper']}>
+        <LogoSVG className={classes['header__logo']} />
+        <h1 className={classes['header__logo-title']}>S3 Bucket Viewer</h1>
+      </div>
+      <div className={classes['header-actions']}>
+        <button type='button' className={classes['header-actions__disconnect']}>
+          <ServerCloseSVG
+            className={classes['header-actions__disconnect-icon']}
+          />
+          <span>Disconnect</span>
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/BucketViewer/BucketViewer.tsx
+++ src/pages/BucketViewer/BucketViewer.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Header from '../../components/UI/Header/Header';
+
+import classes from './BucketViewer.module.css';
+
 const BucketViewer: React.FC = () => {
-  return <>Bucket Viewer</>;
+  return (
+    <div className={classes['bucket-viewer']}>
+      <Header />
+      <main>Folder Tree and WorkingDir</main>
+    </div>
+  );
 };
 
 export default BucketViewer;
